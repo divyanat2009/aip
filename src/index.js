8db2be6a2b523194e8de0d12f21b3834ada9020d
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-30 22:37:13 -0600

    updated data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './_styles/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import DATA from './data'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <App data={DATA}/>
   </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/libraries/fontawesome.js
@@ -0,0 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBookmark, faPodcast, faSeedling, faLink } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCalendarAlt, faLightbulb } from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+  faBookmark, 
+  faPodcast, 
+  faSeedling, 
+  faLink,
+  faHeart,
+  faCalendarAlt,
+  faLightbulb
+);
