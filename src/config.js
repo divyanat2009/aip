8d85120ff4916ca52c72a3c8335b30ecd6153d70
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-01-23 17:01:19 -0600

    endpoint switch

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports= {
-  //API_ENDPOINT: 'https://quiet-shelf-89289.herokuapp.com/api',
+  API_ENDPOINT: 'https://quiet-shelf-89289.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY, 
-  API_ENDPOINT:'http://localhost:3000/'
+  //API_ENDPOINT:'http://localhost:3000/'
    }
    
